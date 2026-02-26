8626a97dd82de7d03415e9bcdac399f4eb78ccf7
andy6609 <andy.lee.madeinseoul@gmail.com>
2026-02-26 16:41:10 +0000

    lazy import setupDevPlatform to fix prod build

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -8,6 +6,7 @@ const nextConfig = {
 };
 
 if (process.env.NODE_ENV === 'development') {
+  const { setupDevPlatform } = await import('@cloudflare/next-on-pages/next-dev');
   await setupDevPlatform();
 }
 
